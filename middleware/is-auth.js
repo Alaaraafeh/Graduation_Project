b4ee5af7c9c7a096371bdabb181310789bb5dd42
Alaaraafeh <alaaraafeh@gmail.com>
2024-04-30 17:49:34 +0300

    .handel the header of the token in is-Auth .the creat post data stord in database

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,11 +1,13 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-    const authHeader = req.get('Authorization');
+    const authHeader = req.get('Authorization'); // get the header
     if(!authHeader){
-        const error = new Error('Not authenticated')
+        const error = new Error('Not authenticated');
+        error.statusCode = 401;
+        throw error;
     }
-    const token = req.get('Authorization').split(' ')[1];
+    const token = authHeader.split(' ')[1]; 
     let decodedToken;
     try {
         decodedToken = jwt.verify(token, 'tokensecret');
